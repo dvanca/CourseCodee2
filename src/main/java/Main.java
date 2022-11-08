ca0a0712f135000ab5328c975ae593d5d2b56959
dvanca <vanca37@gmail.com>
2022-11-08 17:43:45 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,62 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My first maven project !");
+        int number =20;
+        int sum = addNumbers(number);
+        System.out.println("sum is = " + sum);
 
-        Student student = new Student();
-        student.getAge();
-       /// Student student1 = new Student("Maria", 14);
-        Student student2 = Student.builder().age(22).build();
-        Student student3 = Student.builder().age(22).name("Cucu").build();
+        int sum1 = addEvenNumbers(20);
+        System.out.println("sum of even is = " + sum1);
+
+        String string = "kayak";
+        System.out.println("is string " + string + " palindrome? " +" it is  "+ checkPalindrome(string));
+
+        System.out.println(reverseString("masina"));
+
+        System.out.println(sumOfDigits(123));
+    }
+
+    public static int addNumbers(int num){
+        if(num!=0){
+            return num + addNumbers(num-1);
+        }else{
+            return num;
+        }
+    }
+
+    public static int addEvenNumbers(int num){
+        if(num!=0 && num %2 ==0){
+            return num + addEvenNumbers(num-2);
+        }else{
+            return num;
+        }
+    }
+
+    public static boolean checkPalindrome(String s){
+        if(s.length()==0 || s.length()==1){
+            return true;
+        }
+        if(s.charAt(0)==s.charAt(s.length()-1)){
+            return checkPalindrome(s.substring(1, s.length()-1));
+        }
+        return false;
+    }
+
+    public static String reverseString(String s){
+        if(s.length()==0){
+            System.out.println("String is empty");
+            return s;
+        }else{
+            return reverseString(s.substring(1))+s.charAt(0);
+        }
+    }
+
+    public static int sumOfDigits(int n){
+        if(n==0 || n==1){
+            return n;
+        }else{
+            return n%10 + sumOfDigits(n/10);
+        }
     }
 }
