c7ff97355ca94c522e4bb8df17156f156c967a74
dvanca <vanca37@gmail.com>
2022-11-15 10:57:35 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My first maven project !");
+        PersonService personService = new PersonService(List.of(
+                new Person("Mark","Doe",24, "Cluj"),
+                new Person("Anna", "Popescu", 33, "Oradea"),
+                new Person("Mihaela", "Enescu", 43, "Oradea"),
+                new Person("Maria", "Pop", 17, "Cluj"),
+                new Person("Anton", "Avramescu", 53, "Oradea"),
+                new Person("Endel", "Coltescu", 16, "Alba Iulia"),
+                new Person("Anna", "Coltescu", 26, "Alba Iulia")
+        ));
 
-        Student student = new Student();
-        student.getAge();
-       /// Student student1 = new Student("Maria", 14);
-        Student student2 = Student.builder().age(22).build();
-        Student student3 = Student.builder().age(22).name("Cucu").build();
+
+        System.out.println(personService.allPersonsNames());
+        System.out.println(personService.getMajorPersons());
+        System.out.println(personService.getOradeaResidents());
+        System.out.println(personService.getOradeaAndClujResidents());
+        System.out.println(personService.allFirstNamesCapitalized());
+        System.out.println(personService.allFirstNamesPlusLastNameInitial());
+        System.out.println(personService.ageBetweenEighteenAndSixty());
+        System.out.println(personService.personsThatStartWithA());
+        System.out.println(personService.allPersonsUniquely());
+        System.out.println(personService.sortPersonsByFirstName());
+        System.out.println(personService.sortPersonsByLastName());
+        System.out.println(personService.sortPersonsByFirstLastAndAge());
     }
+
 }
---
+++ src/main/java/PersonService.java
@@ -0,0 +1,94 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PersonService {
+    private List<Person> list;
+
+    public PersonService(List<Person> list) {
+        this.list = list;
+    }
+
+    public List<String> allPersonsNames() {
+        return list.stream()
+                .map(Person -> Person.getFirstName() + " " + Person.getLastName())
+                .toList();
+    }
+
+    public List<String> getMajorPersons(){
+        return list.stream()
+                .filter(Person -> Person.getAge() >= 18)
+                .map(Person -> Person.getFirstName() + " " + Person.getLastName())
+                .toList();
+    }
+
+    public List<String> getOradeaResidents(){
+        return list.stream()
+                .filter(Person -> Person.getCity().equals("Oradea"))
+                .map(Person -> Person.getFirstName()+ " " + Person.getLastName())
+                .toList();
+    }
+
+    public List<String> getOradeaAndClujResidents(){
+        return list.stream()
+                .filter(Person -> Person.getCity().equals("Oradea") || Person.getCity().equals("Cluj"))
+                .map(Person -> Person.getFirstName()+ " " + Person.getLastName())
+                .toList();
+
+    }
+
+    public List<String> allFirstNamesCapitalized(){
+        return  list.stream()
+                .map(Person -> Person.getFirstName().toUpperCase())
+                .toList();
+    }
+
+    public List<String> allFirstNamesPlusLastNameInitial(){
+        return list.stream()
+                .map(Person -> Person.getFirstName() + " " + Person.getLastName().charAt(0)+".")
+                .toList();
+    }
+
+    public List<String> ageBetweenEighteenAndSixty(){
+        return list.stream()
+                .filter(Person -> Person.getAge() >18 && Person.getAge() < 60)
+                .map(Person -> Person.getFirstName())
+                .toList();
+    }
+
+    public List<String> personsThatStartWithA(){
+        return list.stream()
+                .map(Person -> Person.getFirstName())
+                .filter(firstName -> firstName.charAt(0) == 'A')
+                .toList();
+    }
+
+    public Set<String> allPersonsUniquely(){
+        return  list.stream()
+                .map(Person -> Person.getFirstName())
+                .collect(Collectors.toSet());
+
+    }
+
+    public List<String> sortPersonsByFirstName(){
+        return list.stream()
+                .map(Person -> Person.getFirstName())
+                .sorted()
+                .toList();
+    }
+
+    public List<String> sortPersonsByLastName(){
+        return list.stream()
+                .map(Person -> Person.getLastName())
+                .sorted()
+                .toList();
+    }
+
+    public List<Person> sortPersonsByFirstLastAndAge(){
+        return  list.stream()
+                .sorted(Comparator.comparing(Person::getFirstName).thenComparing(Person::getLastName).thenComparing(Person::getAge))
+                .toList();
+
+    }
+}
